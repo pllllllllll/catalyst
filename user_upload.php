29d68e39658434613db30d9157c43667cd495356
Paul L <paullindgreen@hotmail.com>
2023-03-28 21:00:20 -0300

    Update user_upload.php

--- user_upload.php
+++ user_upload.php
@@ -26,71 +26,72 @@ if ($Create_Table=='y'){ //create table only, no insert or file reading
 	exit();
 }
 
-
-
-if ($Create_Table=='y'){ //create table only
-	//$Table_Name
-}elseif($Dry_Run=='n'){ //create table, read data, insert data
-		//$Table_Name,$MySQL_Username,$MySQL_Password
-	$options = ['file:'];
-	ftnReadInsertData($options);
-}elseif($Dry_Run=='y'){ //create table, read data, NO insert data
-
-}
-
-
-
-
-
-
-//echo "this is the table name  $a\n";
-//echo "this is the username  $b\n";
-//echo "this is the password  $c\n";
-
 $options = ['file:'];
 $values = getopt(null, $options);
-//$lines = file($values['file'], FILE_IGNORE_NEW_LINES|FILE_SKIP_EMPTY_LINES);
-// Rest of the script...
+$UsersFile = $values['file'];
 
-$row = 1;
-if (($handle = fopen($values['file'], "r")) !== FALSE) {
-  while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
-    $num = count($data);
-    //echo "\n $num fields in line $row: \n";
-    $row++;
-    $ValidEmail='yes';
-   	$ValidNumberOfFields='yes';
 
-    //validate number of fields is 3
-    if ($num<>3) {
-    	//format incorrect for line
-    	echo "\n Number of fields ($num) not 3 for line $row , skipping this line for processing. ".$data[0];
-    }else{
-	    for ($c=0; $c < $num; $c++) {
-	    	//echo "c=$c num=$num /n";
-	    	//clean data: 
-	    	//remove extra spaces
-	        $data[$c] = trim($data[$c]);
-	        //upper case name and surname
-	        if ($c==0 || $c==1) {
-	        	$data[$c] = ucwords(strtolower($data[$c]));
-	        }elseif($c==2){	//email lower case  
-				$data[$c] = strtolower($data[$c]);
-		    	//validate email data, do not insert into mysql otherwise
-				if (preg_match("/^([a-z0-9\+_\-]+)(\.[a-z0-9\+_\-]+)*@([a-z0-9\-]+\.)+[a-z]{2,6}$/ix", $data[$c])) {
-				  //echo 'This is a valid email. '.$data[$c];
-				} else {
-				  echo 'Invalid email. Not processing this record '.$data[$c];
-				  $EmailValid='no';
-				}
-	        }    
-	        //field of data
-	        //echo $data[$c] . "\n";
-	    }
-	}
-  }
-  fclose($handle);
+//create table in all cases
+if (ISSET($ftnTableName)){//create table
+	//ftnCreateTable($ftnTableName,$ftnUsername,$ftnPassword);
+}else{
+	exit(); 
 }
 
+//dry run, create table and read data, no insertion
+//ftnReadData($UsersFile);
+
+//create table, read data, insert data
+//ftnInsertData($UsersFile, $Table_Name, $MySQL_Username, $MySQL_Password);
+
+
+
+
+function ftnInsertData($ftnUsersFile,$ftnTableName,$ftnUsername,$ftnPassword){
+
+	//$lines = file($values['file'], FILE_IGNORE_NEW_LINES|FILE_SKIP_EMPTY_LINES);
+	// Rest of the script...
+
+	$row = 1;
+	if (($handle = fopen($ftnUsersFile, "r")) !== FALSE) {
+		fgetcsv($handle, 10000, ","); //reads first line header, so its skipped in the while loop
+	  while (($data = fgetcsv($handle, 200, ",")) !== FALSE) {
+	    $num = count($data);
+	    //echo "\n $num fields in line $row: \n";
+	    $row++;
+	    $ValidEmail='yes';
+	   	$ValidNumberOfFields='yes';
+
+	    //validate number of fields is 3
+	    if ($num<>3) {
+	    	//format incorrect for line
+	    	echo "\n Number of fields ($num) not 3 for line $row , skipping this line for processing. ".$data[0];
+	    }else{
+		    for ($c=0; $c < $num; $c++) {
+		    	//echo "c=$c num=$num /n";
+		    	//clean data: 
+		    	//remove extra spaces
+		        $data[$c] = trim($data[$c]);
+		        //upper case name and surname
+		        if ($c==0 || $c==1) {
+		        	$data[$c] = ucwords(strtolower($data[$c]));
+		        }elseif($c==2){	//email lower case  
+					$data[$c] = strtolower($data[$c]);
+			    	//validate email data, do not insert into mysql otherwise
+					if (preg_match("/^([a-z0-9\+_\-]+)(\.[a-z0-9\+_\-]+)*@([a-z0-9\-]+\.)+[a-z]{2,6}$/ix", $data[$c])) {
+					  //echo 'This is a valid email. '.$data[$c];
+					} else {
+					  echo "\n Invalid email. Not processing this record ".$data[$c];
+					  $EmailValid='no';
+					}
+		        }    
+		        //field of data
+		        //echo $data[$c] . "\n";
+		    }
+		}
+	  }
+	  fclose($handle);
+	}
+}// function ftnReadData($ftnOptions){
 ?>
 
