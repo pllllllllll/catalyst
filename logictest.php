be25b25151e527179ba5d203e258bd02378b2a43
Paul L <paullindgreen@hotmail.com>
2023-03-29 22:52:22 -0300

    Update logictest.php

--- logictest.php
+++ logictest.php
@@ -2,11 +2,20 @@
 //C:\xampp\php\php.exe C:\xampp\htdocs\catalyst\catalyst\logictest.php  
 for ($c=1; $c < 101; $c++) {
 $three = $c/3;
+echo "$c ";
 if (is_float($three)) {
-  echo '$three is a decimal number';
+  //echo "$c/3 = $three is a decimal number\n";
 } else {
-  echo '$three is an integer';
+  echo "foo";
 }
+
+$five = $c/5;
+if (is_float($five)) {
+  //echo "$c/5 = $five is a decimal number\n";
+} else {
+  echo "bar";
+}
+
 
 }
 ?>
